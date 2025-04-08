790728ec96cebd9a0eec928f070122009a86a0e2
Ando <hi@andostronaut.com>
2025-04-08 11:53:44 +0300

    feat: add instatus frame and redirect to instatus (#151)

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,23 @@ import { Heart } from '@/components/icons/heart'
 export const Footer = () => {
   return (
     <footer className="absolute bottom-0 left-0 w-full p-6">
-      <div className="flex items-center space-x-3 text-gray-400">
-        <div className="h-6 w-6 opacity-50">
-          <Heart />
+      <div className="flex justify-between items-center">
+        <div className="flex items-center space-x-3 text-gray-400">
+          <div className="h-6 w-6 opacity-50">
+            <Heart />
+          </div>
+          <p className="text-sm">crafted with care and dedication.</p>
         </div>
-        <p className="text-sm">crafted with care and dedication.</p>
+        <div>
+          <iframe
+            src="https://kinotio.instatus.com/embed-status/6f8e5fad/light-sm"
+            width="230"
+            height="45"
+            frameBorder="0"
+            scrolling="no"
+            style={{ border: 'none' }}
+          />
+        </div>
       </div>
     </footer>
   )
